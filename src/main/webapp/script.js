698a0f7f19dec0f1e917bac7ed4b29c36f74d51d
Tzirliya <tzirliya@yahoo.com>
2020-07-21 18:39:16 +0000

    Make buttons for admins, tasks, and members on the Project Page.

---
+++ src/main/webapp/script.js
@@ -0,0 +1,11 @@
+function goToUser() {
+  // TODO: fetch for actual user's page
+  var url = "user_profile.html";
+  location.href = url;
+}
+
+function goToTask() {
+  // TODO: fetch for actual user's page
+  var url = "task.html";
+  location.href = url;
+}
