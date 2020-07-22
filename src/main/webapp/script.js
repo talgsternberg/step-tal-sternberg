0c1eb3a2920051e7d4f88572032ef27687d52a01
Tzirliya <tzirliya@yahoo.com>
2020-07-22 19:47:25 +0000

    Task name, description, and status appear from random hardcoded tasks.

--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -7,11 +7,44 @@ function goToUser() {
   location.href = url;
 }
 
+const g_tasks = {task1:{taskID:1, projectID:1, name:"Task 1", description:"This is task 1", status:"incomplete"},
+                task2:{taskID:2, projectID:1, name:"Task 2", description:"This is task 2", status:"complete"},
+                task3:{taskID:3, projectID:1, name:"Task 3", description:"This is task 3", status:"incomplete"}};
+const g_JSONtasks = JSON.stringify(g_tasks);
+console.log(g_tasks);
+console.log(g_JSONtasks);
+
 /**
  * Redirect to Task Page
  */
 function goToTask() {
-  // TODO: fetch for actual task's page
-  const url = 'task.html';
+  const taskID = Math.floor(Math.random() * (4 - 1) ) + 1;
+  const url = 'task.html?taskID=' + taskID;
   location.href = url;
+//   let taskID = Math.floor(Math.random() * (4 - 1) ) + 1;
+//   console.log(taskID);
+  //document.body.onload = getTaskInfo(1);
+}
+
+function getTaskInfo() {
+  //let taskID = 1;
+  const params = new URLSearchParams(location.search);
+  let taskID = params.get("taskID");
+  console.log(taskID);
+  let tasks = JSON.parse(g_JSONtasks);
+  // TODO: fetch for actual task's page
+  for (task in tasks){
+    console.log(task);
+    console.log(tasks[task].description);
+    if (tasks[task].taskID == taskID){
+      console.log("Entered if statement")
+      let title = document.getElementById("task-title-container");
+      title.innerHTML = "<h1>" + tasks[task].name + "</h1>";
+      let description = document.getElementById("task-description-container");
+      description.innerText = tasks[task].description;
+      let status = document.getElementById("task-status-container");
+      status.innerText = "Status: " + tasks[task].status;
+      break;
+    }
+  }
 }
