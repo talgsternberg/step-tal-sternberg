f92c93a606c40bca125f1082edd8bddc6a7b530c
Tzirliya <tzirliya@yahoo.com>
2020-07-22 20:07:32 +0000

    Task buttons from Project and Task Pages will direct to corresponding Task Page.

--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -1,5 +1,5 @@
 /**
- * Redirect to User Profile Page
+ * Redirect to User Profile Page.
  */
 function goToUser() {
   // TODO: fetch for actual user's page
@@ -7,37 +7,30 @@ function goToUser() {
   location.href = url;
 }
 
+/**
+ * Redirect to Task Page. If no taskID is provided, randomize it from 1 to 3 inclusive.
+ */
+function goToTask(taskID=Math.floor(Math.random()*(3))+1) {
+  const url = 'task.html?taskID=' + taskID;
+  location.href = url;
+}
+
+// Hard coded tasks that are global variables.
 const g_tasks = {task1:{taskID:1, projectID:1, name:"Task 1", description:"This is task 1", status:"incomplete"},
                 task2:{taskID:2, projectID:1, name:"Task 2", description:"This is task 2", status:"complete"},
                 task3:{taskID:3, projectID:1, name:"Task 3", description:"This is task 3", status:"incomplete"}};
 const g_JSONtasks = JSON.stringify(g_tasks);
-console.log(g_tasks);
-console.log(g_JSONtasks);
 
 /**
- * Redirect to Task Page
+ * When the Task Page loads, get task info. If no taskID is provided in the URL,
+ * default values will be shown.
  */
-function goToTask() {
-  const taskID = Math.floor(Math.random() * (4 - 1) ) + 1;
-  const url = 'task.html?taskID=' + taskID;
-  location.href = url;
-//   let taskID = Math.floor(Math.random() * (4 - 1) ) + 1;
-//   console.log(taskID);
-  //document.body.onload = getTaskInfo(1);
-}
-
 function getTaskInfo() {
-  //let taskID = 1;
   const params = new URLSearchParams(location.search);
   let taskID = params.get("taskID");
-  console.log(taskID);
   let tasks = JSON.parse(g_JSONtasks);
-  // TODO: fetch for actual task's page
   for (task in tasks){
-    console.log(task);
-    console.log(tasks[task].description);
     if (tasks[task].taskID == taskID){
-      console.log("Entered if statement")
       let title = document.getElementById("task-title-container");
       title.innerHTML = "<h1>" + tasks[task].name + "</h1>";
       let description = document.getElementById("task-description-container");
