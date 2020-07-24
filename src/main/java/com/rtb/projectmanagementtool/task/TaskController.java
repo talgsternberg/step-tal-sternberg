d2389706c2857b7bf1b45410a470afd902386fd0
Tzirliya <tzirliya@yahoo.com>
2020-07-24 20:43:43 +0000

    Fix code format.

--- src/main/java/com/rtb/projectmanagementtool/task/TaskController.java
+++ src/main/java/com/rtb/projectmanagementtool/task/TaskController.java
@@ -6,7 +6,6 @@ import com.google.appengine.api.datastore.Entity;
 import com.google.appengine.api.datastore.PreparedQuery;
 import com.google.appengine.api.datastore.Query;
 import com.google.appengine.api.datastore.Key;
-
 import java.util.HashSet;
 
 /** Class controlling the TaskData object. */
