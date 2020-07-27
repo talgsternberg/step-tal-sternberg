3c95c5d71e8719b7b5800e3e0e58b465351b12dd
Tal <talsternberg@google.com>
2020-07-27 19:11:11 +0000

    lint fix

--- src/main/java/com/rtb/projectmanagementtool/user/UserController.java
+++ src/main/java/com/rtb/projectmanagementtool/user/UserController.java
@@ -16,13 +16,13 @@ public final class UserController {
     this.users = users;
   }
 
-  //get users+data to direct/display user's profiles later
+  // get users+data to direct/display user's profiles later
   public HashSet<UserData> getEveryUser() {
     DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
     HashSet<UserData> users = new HashSet<>();
     Query query = new Query("User");
     PreparedQuery results = datastore.prepare(query);
-    //create new user object and add to users
+    // create new user object and add to users
     for (Entity entity : results.asIterable()) {
       long userID = (long) entity.getKey().getId();
       String userEmail = (String) entity.getProperty("userEmail");
