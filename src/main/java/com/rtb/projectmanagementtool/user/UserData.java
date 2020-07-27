79793f7cad8e504ef05617e21ef644749db0913e
Tal <talsternberg@google.com>
2020-07-27 20:07:35 +0000

    self creating object w/ entity in UserData class

--- src/main/java/com/rtb/projectmanagementtool/user/UserData.java
+++ src/main/java/com/rtb/projectmanagementtool/user/UserData.java
@@ -16,7 +16,7 @@ enum Skills {
 public final class UserData {
 
   private long userID;
-  private String userEmail;
+  private long AuthID; // this will be the ID from API
   private String userName;
   private int userYear;
   private HashSet<String> userMajors;
@@ -25,14 +25,14 @@ public final class UserData {
 
   public UserData(
       long userID,
-      String userEmail,
+      long AuthID,
       String userName,
       int userYear,
       HashSet<String> userMajors,
       Skills skills,
       int userTotalCompTasks) {
     this.userID = userID;
-    this.userEmail = userEmail;
+    this.AuthID = AuthID;
     this.userName = userName;
     this.userYear = userYear;
     this.userMajors = userMajors;
@@ -40,12 +40,33 @@ public final class UserData {
     this.userTotalCompTasks = userTotalCompTasks;
   }
 
+  public UserData(Entity entity) {
+    userID = (long) entity.getKey().getId();
+    AuthID = (long) entity.getProperty("AuthID");
+    userName = (String) entity.getProperty("userName");
+    userYear = (int) entity.getProperty("userYear");
+    userMajors = (HashSet<String>) entity.getProperty("userMajors");
+    skills = (Skills) entity.getProperty("skills");
+    userTotalCompTasks = (int) entity.getProperty("userTotalCompTasks")
+  }
+
+  public Entity toEntity() {
+    Entity entity = new Entity("User", userID);
+    entity.setProperty("AuthID", AuthID);
+    entity.setProperty("userName", userName);
+    entity.setProperty("userYear", userYear);
+    entity.setProperty("userMajors", userMajors);
+    entity.setProperty("skills", skills);
+    entity.setProperty("userTotalCompTasks", userTotalCompTasks);
+    return entity;
+  }
+
   public long getUserID() {
     return userID;
   }
 
-  public String getUserEmail() {
-    return userEmail;
+  public long getAuthID() {
+    return AuthID;
   }
 
   public String getUserName() {
@@ -72,8 +93,8 @@ public final class UserData {
     this.userID = userID;
   }
 
-  public void setUserEmail(String userEmail) {
-    this.userEmail = userEmail;
+  public void setUserEmail(long AuthID) {
+    this.AuthID = AuthID;
   }
 
   public void setUserName(String userName) {
