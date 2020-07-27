c0751cecd4af364ee81d2b3ca3990c6a5aa628a4
John <jespejo@google.com>
2020-07-27 21:54:18 +0000

    add local Datastore test classes

---
+++ src/main/java/com/rtb/projectmanagementtool/taskblocker/TaskBlockerData.java
@@ -0,0 +1,37 @@
+package com.rtb.projectmanagementtool.taskblocker;
+
+import com.google.appengine.api.datastore.Entity;
+
+/** Class containing task blocker data. */
+public final class TaskBlockerData {
+
+  private long taskID;
+  private long blockerID;
+
+  public TaskBlockerData(
+      long taskID,
+      long blockerID) {
+    this.taskID = taskID;
+    this.blockerID = blockerID;
+  }
+
+  public TaskBlockerData(Entity entity) {
+    taskID = (long) entity.getProperty("taskID");
+    blockerID = (long) entity.getProperty("blockerID");
+  }
+
+  public Entity toEntity() {
+    Entity entity = new Entity("TaskBlocker");
+    entity.setProperty("taskID", taskID);
+    entity.setProperty("blockerID", blockerID);
+    return entity;
+  }
+
+  public long getTaskID() {
+    return taskID;
+  }
+
+  public long getBlockerID() {
+    return blockerID;
+  }
+}
---
+++ src/test/java/com/rtb/projectmanagementtool/taskblocker/TaskBlockerControllerTest.java
@@ -0,0 +1,82 @@
+package com.rtb.projectmanagementtool.taskblocker;
+
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.dev.LocalDatastoreService;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.tools.development.testing.LocalDatastoreServiceTestConfig;
+import com.google.appengine.tools.development.testing.LocalServiceTestHelper;
+import java.util.HashSet;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TaskBlockerControllerTest {
+    private final LocalServiceTestHelper helper =
+      new LocalServiceTestHelper(
+          new LocalDatastoreServiceTestConfig()
+          .setAutoIdAllocationPolicy(LocalDatastoreService.AutoIdAllocationPolicy.SEQUENTIAL));
+
+    @Before
+    public void setUp() {
+        helper.setUp();
+    }
+
+    @After
+    public void tearDown() {
+        helper.tearDown();
+    }
+
+    @Test
+    public void testAddTaskBlockers() {
+        DatastoreService ds = DatastoreServiceFactory.getDatastoreService();
+        TaskBlockerController ctl = new TaskBlockerController();
+
+        // Add 2 initially
+        ctl.addTaskBlocker(ds, /*taskID=*/ 1l, /*blockerID=*/ 2l);
+        ctl.addTaskBlocker(ds, /*taskID=*/ 2l, /*blockerID=*/ 3l);
+
+        // Should have 2 now
+        HashSet<TaskBlockerData> blockers = ctl.getTaskBlockers(ds, /*taskID=*/ 1l);
+        Assert.assertEquals(blockers.size(), 2);
+
+        // Add 2 more
+        ctl.addTaskBlocker(ds, /*taskID=*/ 1l, /*blockerID=*/ 2l);
+        ctl.addTaskBlocker(ds, /*taskID=*/ 2l, /*blockerID=*/ 3l);
+
+        // Should be 4 now
+        blockers = ctl.getTaskBlockers(ds, /*taskID=*/ 1l);
+        Assert.assertEquals(blockers.size(), 4);
+    }
+
+    @Test
+    public void testGetBlockersById() {
+        DatastoreService ds = DatastoreServiceFactory.getDatastoreService();
+        TaskBlockerController ctl = new TaskBlockerController();
+
+        // 2 blockers for taskID=1
+        ctl.addTaskBlocker(ds, /*taskID=*/ 1l, /*blockerID=*/ 2l);
+        ctl.addTaskBlocker(ds, /*taskID=*/ 1l, /*blockerID=*/ 3l);
+
+
+        // 5 blockers for taskID=2
+        ctl.addTaskBlocker(ds, /*taskID=*/ 2l, /*blockerID=*/ 4l);
+        ctl.addTaskBlocker(ds, /*taskID=*/ 2l, /*blockerID=*/ 5l);
+        ctl.addTaskBlocker(ds, /*taskID=*/ 2l, /*blockerID=*/ 6l);
+
+        HashSet<TaskBlockerData> blockers = ctl.getTaskBlockers(ds, /*taskID=*/ 1l);
+        Assert.assertFalse(blockers.isEmpty());
+        
+        /*
+        // These two asserts will fail because getTaskBlockers filter by
+        // taskID has not been implemented yet
+
+        Assert.assertEquals(blockers.size(), 3);
+
+        for (TaskBlockerData blocker : blockers) {
+            Assert.assertEquals(blocker.getTaskID(), 1l);
+        }
+        */
+    }
+}
