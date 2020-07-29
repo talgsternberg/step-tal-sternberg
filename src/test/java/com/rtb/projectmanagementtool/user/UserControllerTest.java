34368dcc98fa9a4a0e4cd508edcb6f8b0d47003f
Tal <talsternberg@google.com>
2020-07-29 18:15:20 +0000

    more lint

--- src/test/java/com/rtb/projectmanagementtool/user/UserControllerTest.java
+++ src/test/java/com/rtb/projectmanagementtool/user/UserControllerTest.java
@@ -1,12 +1,14 @@
 package com.rtb.projectmanagementtool.user;
 
-import com.google.appengine.api.datastore.DatastoreService;
-import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import static com.google.appengine.api.datastore.FetchOptions.Builder.withLimit;
+
+import com.google.appengine.api.datastore.*;
+import com.google.appengine.api.datastore.Query.*;
 import com.google.appengine.api.datastore.dev.LocalDatastoreService;
-import com.google.appengine.api.datastore.Entity;
 import com.google.appengine.tools.development.testing.LocalDatastoreServiceTestConfig;
 import com.google.appengine.tools.development.testing.LocalServiceTestHelper;
-import java.util.*;
+import java.util.Arrays;
+import java.util.HashSet;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
@@ -15,42 +17,42 @@ import org.junit.Test;
 public class UserControllerTest {
   private HashSet<UserData> testUsers;
   private final LocalServiceTestHelper helper =
-    new LocalServiceTestHelper(
-      new LocalDatastoreServiceTestConfig()
-        .setAutoIdAllocationPolicy(LocalDatastoreService.AutoIdAllocationPolicy.SEQUENTIAL));
+      new LocalServiceTestHelper(
+          new LocalDatastoreServiceTestConfig()
+              .setAutoIdAllocationPolicy(LocalDatastoreService.AutoIdAllocationPolicy.SEQUENTIAL));
 
-    @Before
-    public void setUp() {
-      helper.setUp();
-    }
+  @Before
+  public void setUp() {
+    helper.setUp();
+  }
 
-    @After
-    public void tearDown() {
-      helper.tearDown();
-    }
+  @After
+  public void tearDown() {
+    helper.tearDown();
+  }
 
-    @Test
-    public void testAddUsers() {
-      DatastoreService ds = DatastoreServiceFactory.getDatastoreService();
-      UserController ctl = new UserController(testUsers);
-      ArrayList<String> user1Majors = new ArrayList<String>();
-      ArrayList<String> user2Majors = new ArrayList<String>();
-      user1Majors.add("Biology");
-      user2Majors.add("English");
-      user2Majors.add("Religion");
-      // Add 2 initially
-      /* ctl.addUser(
-        ds, new UserData(1l, 2l, "Sarah", 2022,
-        user1Majors, Skills.LEADERSHIP.name(), 3));*/
-      ctl.addUser(
-        ds, new UserData(1l, 2l, "Sarah", 2023,
-        user1Majors, 5));
-      ctl.addUser(
-        ds, new UserData(2l, 3l, "Joe", 2022,
-        user2Majors, 3));
+  @Test
+  public void testAddUsers() {
+    DatastoreService ds = DatastoreServiceFactory.getDatastoreService();
+    UserController ctl = new UserController(testUsers);
+    ArrayList<String> user1Majors = new ArrayList<String>();
+    ArrayList<String> user2Majors = new ArrayList<String>();
+    user1Majors.add("Biology");
+    user2Majors.add("English");
+    user2Majors.add("Religion");
+    // Add 2 initially
+    /* ctl.addUser(
+      ds, new UserData(1l, 2l, "Sarah", 2022,
+      user1Majors, Skills.LEADERSHIP.name(), 3));*/
+    ctl.addUser(
+      ds, new UserData(1l, 2l, "Sarah", 2023,
+      user1Majors, 5));
+    ctl.addUser(
+      ds, new UserData(2l, 3l, "Joe", 2022,
+      user2Majors, 3));
 
-      // Should have 2 now
-      HashSet<UserData> users = ctl.getEveryUser(ds);
-      Assert.assertEquals(users.size(), 2);
-    }
+    // Should have 2 now
+    HashSet<UserData> users = ctl.getEveryUser(ds);
+    Assert.assertEquals(users.size(), 2);
+  }
 } 
