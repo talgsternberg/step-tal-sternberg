3447a1b51942c2c3dd757b7a247a301149e3bb08
Tal <talsternberg@google.com>
2020-07-29 19:57:03 +0000

    more lint errors

--- src/test/java/com/rtb/projectmanagementtool/user/UserControllerTest.java
+++ src/test/java/com/rtb/projectmanagementtool/user/UserControllerTest.java
@@ -38,9 +38,6 @@ public class UserControllerTest {
     user2Majors.add("English");
     user2Majors.add("Religion");
     // Add 2 initially
-    /* ctl.addUser(
-      ds, new UserData(1l, 2l, "Sarah", 2022,
-      user1Majors, Skills.LEADERSHIP.name(), 3));*/
     ctl.addUser(ds, new UserData(1l, 2l, "Sarah", 2023, user1Majors, 5));
     ctl.addUser(ds, new UserData(2l, 3l, "Joe", 2022, user2Majors, 3));
 
@@ -48,5 +45,4 @@ public class UserControllerTest {
     HashSet<UserData> users = ctl.getEveryUser(ds);
     Assert.assertEquals(users.size(), 2);
   }
-
 } 
