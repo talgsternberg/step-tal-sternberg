7b7ba3d8c4e0eb96753f5552b49e8ca0a50d98f8
Tzirliya <tzirliya@yahoo.com>
2020-07-29 20:17:05 +0000

    Fix code format.

--- src/test/java/com/rtb/projectmanagementtool/task/TaskControllerTest.java
+++ src/test/java/com/rtb/projectmanagementtool/task/TaskControllerTest.java
@@ -9,8 +9,6 @@ import com.google.appengine.tools.development.testing.LocalDatastoreServiceTestC
 import com.google.appengine.tools.development.testing.LocalServiceTestHelper;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashSet;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
