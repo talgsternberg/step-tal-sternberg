78378b11f1f20cac78048449f7f199c3e3f60e8b
Tal <talsternberg@google.com>
2020-07-31 12:48:33 +0000

    modify Controller and Servlet. Servlet Error.

--- src/main/java/com/rtb/projectmanagementtool/user/UserController.java
+++ src/main/java/com/rtb/projectmanagementtool/user/UserController.java
@@ -2,26 +2,27 @@ package com.rtb.projectmanagementtool.user;
 
 import com.google.appengine.api.datastore.*;
 import com.google.appengine.api.datastore.Query.*;
-import java.util.HashSet;
+import java.util.ArrayList;
 
 public final class UserController {
 
   private DatastoreService datastore;
 
-  public UserController(Datastore datastore) {
+  public UserController(DatastoreService datastore) {
     this.datastore = datastore;
   }
 
   public UserData getUserByID(long userID) {
-    Query query = new Query("User").addFilter("userID", FilterOperator.EQUAL, taskID);
+    Query query = new Query("User").addFilter("userID", FilterOperator.EQUAL, userID);
     PreparedQuery results = datastore.prepare(query);
     Entity entity = results.asSingleEntity();
     UserData user = new UserData(entity);
+    return user;
   }
 
   // get users+data to direct/display user's profiles later
-  public HashSet<UserData> getEveryUser() {
-    HashSet<UserData> users = new HashSet<>();
+  public ArrayList<UserData> getEveryUser() {
+    ArrayList<UserData> users = new ArrayList<>();
     Query query = new Query("User");
     PreparedQuery results = datastore.prepare(query);
     // create new user object and add to users
--- src/main/java/com/rtb/projectmanagementtool/user/UserServlet.java
+++ src/main/java/com/rtb/projectmanagementtool/user/UserServlet.java
@@ -5,28 +5,37 @@ import com.google.gson.Gson;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 
 @WebServlet("/user")
 public class UserServlet extends HttpServlet {
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    Query query = new Query("User");
 
     DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
-    PreparedQuery results = datastore.prepare(query);
+
+    // Test user
+    // long userID1 = 1l;
+    // long AuthID1 = 1l;
+    // String userName1 = "Jake";
+    // long userYear1 = 2023;
+    // ArrayList<String> userMajors1 = new ArrayList<>(Arrays.asList("Geology", "Physics"));
+    // long userTotalCompTasks1 = 8;
+    // UserData user1 =
+    // new UserData(userID1, AuthID1, userName1, userYear1, userMajors1, userTotalCompTasks1);
 
     // get the User
     long userID = Long.parseLong(request.getParameter("userID"));
     UserController userController = new UserController(datastore);
     UserData user = UserController.getUserByID(userID);
+
+    // add to list of users
     ArrayList<UserData> usersList = new ArrayList<>(Arrays.asList(user));
+    // ArrayList<UserData> usersList = new ArrayList<>(Arrays.asList(user1));
 
     // convert to JSON
     Gson gson = new Gson();
@@ -36,21 +45,23 @@ public class UserServlet extends HttpServlet {
 
   @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-      Entity entity = new Entity("User");
-      
-      // params from request
-      long userID = (long) entity.getKey().getId();
-      long AuthID = Long.parseLong(request.getParameter("AuthID"));
-      String userName = request.getParameter("userName").trim();
-      long userYear = Long.parseLong(request.getParameter("userYear"));
-      ArrayList<String> userMajors= (ArrayList<String>) request.getParameter("userMajors").trim();
-      long userTotal = Long.parseLong(request.getParameter("userTotalCompTasks"));
-      
-      //create new user. Set up datastore
-      UserData user = new UserData(userID, AuthID, userName, userYear, userMajors, userTotal);
-      DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
-      
-      // new UserController. Add UserData to it.
-      UserController userController = new UserController(datastore);
-      userController.addUser(user);
+    Entity entity = new Entity("User");
+
+    // params from request
+    long userID = (long) entity.getKey().getId();
+    long AuthID = Long.parseLong(request.getParameter("AuthID"));
+    String userName = request.getParameter("userName").trim();
+    long userYear = Long.parseLong(request.getParameter("userYear"));
+    ArrayList<String> userMajors =
+        (ArrayList<String>) Arrays.asList(request.getParameterValues("userMajors"));
+    long userTotal = Long.parseLong(request.getParameter("userTotalCompTasks"));
+
+    // create new user. Set up datastore
+    UserData user = new UserData(userID, AuthID, userName, userYear, userMajors, userTotal);
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+
+    // new UserController. Add UserData to it.
+    UserController userController = new UserController(datastore);
+    userController.addUser(user);
+  }
 }
