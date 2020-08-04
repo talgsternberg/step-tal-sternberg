902e2b1c4afdb1078e41a44b5aae60e5c7c553a1
Tal <talsternberg@google.com>
2020-08-04 19:31:00 +0000

    create AuthServlet for user authentication and an empty test class

---
+++ src/main/java/com/rtb/projectmanagementtool/user/AuthServlet.java
@@ -0,0 +1,91 @@
+package com.rtb.projectmanagementtool.user;
+
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
+import com.google.gson.Gson;
+import com.google.sps.data.Log;
+import java.io.IOException;
+import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/user")
+public class UserServlet extends HttpServlet {
+
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    UserService userService = UserServiceFactory.getUserService();
+
+    List<Log> Logs = new ArrayList<>();
+    String status; // logged in or out
+    String userEmail;
+    long AuthID; // ID to track user for storing their info in datastore
+    String urlToRedirectToAfterUserLogsIn; // go to index (all user data should load)
+    String urlToRedirectToAfterUserLogsOut; // go to index with other pages blocked
+    String loginUrl;
+    String logoutUrl;
+
+    // get all the properties
+    if (userService.isUserLoggedIn()) {
+      status = "In";
+      userEmail = userService.getCurrentUser().getEmail();
+      AuthID = userService.getCurrentUser().getId();
+      urlToRedirectToAfterUserLogsOut = "/";
+      urlToRedirectToAfterUserLogsIn =
+          "/"; // somehow modify to load scripts for user's specific data on pages
+      logoutUrl = userService.createLogoutURL(urlToRedirectToAfterUserLogsOut);
+      loginUrl = userService.createLoginURL(urlToRedirectToAfterUserLogsIn);
+    } else {
+      status = "Out";
+      urlToRedirectToAfterUserLogsOut = "/";
+      urlToRedirectToAfterUserLogsIn = "/";
+      loginUrl = userService.createLoginURL(urlToRedirectToAfterUserLogsIn);
+      logoutUrl = userService.createLogoutURL(urlToRedirectToAfterUserLogsOut);
+      userEmail = ""; // empty email
+      AuthID = 0; // "empty" AuthID
+    }
+
+    // create new log object
+    Log log = new Log(status, loginUrl, logoutUrl, userEmail, AuthID);
+
+    // add to Logs which stores all users
+    Logs.add(log);
+
+    // write using gson
+    Gson gson = new Gson();
+    response.setContentType("application/json;");
+    response.getWriter().println(gson.toJson(Logs));
+  }
+
+  @Override
+  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    // get each value from the request
+    String status = request.getParameter("status");
+    String loginUrl = request.getParameter("loginUrl");
+    String logoutUrl = request.getParameter("logoutUrl");
+    String userEmail = request.getParameter("userEmail");
+    String userEmail = request.getParameter("AuthID");
+
+    // setup new Entity to store
+    Entity logEntity = new Entity("Log");
+
+    // set properties to be stored
+    logEntity.setProperty("status", status);
+    logEntity.setProperty("loginUrl", loginUrl);
+    logEntity.setProperty("logoutUrl", logoutUrl);
+    logEntity.setProperty("userEmail", userEmail);
+    logEntity.setProperty("AuthID", AuthID);
+
+    // store data
+    // will use AuthID (in both login and UserData to connect info)
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    datastore.put(logEntity);
+  }
+}
