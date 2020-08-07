e8a6b54d84f50e9d1c6e7cd2209f5c257005a680
Tal <talsternberg@google.com>
2020-08-07 16:14:12 +0000

    updates to tests. Already logged test working

--- src/test/java/com/rtb/projectmanagementtool/auth/AuthOpsTest.java
+++ src/test/java/com/rtb/projectmanagementtool/auth/AuthOpsTest.java
@@ -23,7 +23,7 @@ public class AuthOpsTest {
   private AuthOps auth;
 
   private final LocalServiceTestHelper helper =
-      new LocalServiceTestHelper(new LocalUserServiceTestConfig())
+      new LocalServiceTestHelper(new LocalUserServiceTestConfig().setOAuthUserId("abc"))
           .setEnvIsAdmin(true)
           .setEnvIsLoggedIn(true);
 
@@ -68,44 +68,66 @@ public class AuthOpsTest {
     auth.loginUser(request, response);
 
     // captor setup
-    ArgumentCaptor<Cookie> cookieCaptor = ArgumentCaptor.forClass(Cookie.class);
     verify(response, never()).addCookie(any());
-    Cookie cookie = cookieCaptor.getValue();
 
-    Assert.assertEquals(null, cookie);
+    // Assert.assertEquals(null, cookie);
   }
 
   @Test
   public void testLoginUser() {
+
+    System.out.println("Checkpoint 1");
+
     // add list of mock users
     ArrayList<UserData> testUsers = new ArrayList<UserData>();
     testUsers.add(new UserData(1l, "abc"));
     testUsers.add(new UserData(2l, "opq"));
     testUsers.add(new UserData(3l, "xyz"));
 
+    System.out.println("Checkpoint 2");
+
     // new controller
     UserController controller = mock(UserController.class);
+
+    System.out.println("Checkpoint 3");
 
     // build and send a test cookie for logged out user
     Cookie[] testCookies = new Cookie[1];
     testCookies[0] = new Cookie("SessionUserID", "-1");
 
+    System.out.println("Checkpoint 4");
+
     // on this call in class method, return test user
     when(controller.getEveryUser()).thenReturn(testUsers);
+
+    System.out.println("Checkpoint 5");
 
     // when get cookies is called, pass cookies
     when(request.getCookies()).thenReturn(testCookies);
 
+    System.out.println("Checkpoint 6");
+
     // call loginUser
     auth.loginUser(request, response);
 
+    System.out.println("Checkpoint 7");
+
     // captor setup
     ArgumentCaptor<Cookie> cookieCaptor = ArgumentCaptor.forClass(Cookie.class);
-    verify(response)
-        .addCookie(
-            cookieCaptor
-                .capture()); // this line is still erroring (even when I add cookie with value "out"
+
+    System.out.println("Checkpoint 8");
+
+    verify(response).addCookie(cookieCaptor.capture());
+
+    System.out.println("Checkpoint 9");
+
     Cookie cookie = cookieCaptor.getValue();
+
+    // prints to debug (none print)
+    System.out.println("from captor: ");
+    System.out.println(cookie.getValue());
+    System.out.println("from test cookies: ");
+    System.out.println(testCookies[0].getValue());
 
     Assert.assertEquals(cookie.getValue(), testCookies[0].getValue());
   }
