6922017b349d79734cea05bd158deaae467d9516
Godsfavour <godsfavour@google.com>
2020-08-19 17:27:28 +0000

    fix lint

--- src/main/webapp/scripts/project.js
+++ src/main/webapp/scripts/project.js
@@ -1,10 +1,9 @@
 function showAddUserForm() {
-  document.getElementById("project-add-user-form").style.display = "block";
-  document.getElementById("add-user-button").style.display = "none";
+  document.getElementById('project-add-user-form').style.display = 'block';
+  document.getElementById('add-user-button').style.display = 'none';
 }
 
 function hideAddUserForm() {
-  document.getElementById("project-add-user-form").style.display = "none";
-  document.getElementById("add-user-button").style.display = "block";
+  document.getElementById('project-add-user-form').style.display = 'none';
+  document.getElementById('add-user-button').style.display = 'block';
 }
-
