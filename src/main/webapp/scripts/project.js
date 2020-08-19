4983708a044cd054327eea2b0242d3494f802fb9
Godsfavour <godsfavour@google.com>
2020-08-19 17:41:53 +0000

    ..

--- src/main/webapp/scripts/project.js
+++ src/main/webapp/scripts/project.js
@@ -2,13 +2,17 @@
  * This file contains javascript code for the project page
  */
  
- /* Method that shows the add user form */
- function showAddUserForm() {
+/**
+ * Method that shows the add user form 
+ */
+function showAddUserForm() {
   document.getElementById('project-add-user-form').style.display = 'block';
   document.getElementById('add-user-button').style.display = 'none';
 }
 
-/*Method that hides the add user form */
+/*
+ * Method that hides the add user form 
+ */
 function hideAddUserForm() {
   document.getElementById('project-add-user-form').style.display = 'none';
   document.getElementById('add-user-button').style.display = 'block';
